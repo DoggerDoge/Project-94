b0b5c6004d5cba35c91c9044fd6aac76a715056e
DoggerDoge <97719518+DoggerDoge@users.noreply.github.com>
2022-10-06 13:25:22 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIq8sy7Q0M_K-BM9V5GmHJiQFrNlcjNaY",
+    authDomain: "kwitter-project-thingy7.firebaseapp.com",
+    projectId: "kwitter-project-thingy7",
+    storageBucket: "kwitter-project-thingy7.appspot.com",
+    messagingSenderId: "788340583247",
+    appId: "1:788340583247:web:d5aa865193c7da0ccff33a",
+    measurementId: "G-NQW1TW6BFX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
